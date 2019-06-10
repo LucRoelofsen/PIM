66021123fff65cd2940b6d362f8c3e00f5806a9d
LucRoelofsen <51378779+LucRoelofsen@users.noreply.github.com>
2019-06-11 00:53:32 +0200

    added wheelzoom for images + scripts to some html files

--- public/webapp/js/rijkscrowd.js
+++ public/webapp/js/rijkscrowd.js
@@ -1,1 +1,17 @@
+// highlights current item in navbar
 $('body').scrollspy({ target: '#navbar-example' })
+
+// assign wheelzoom
+wheelzoom(document.querySelectorAll('img'));
+
+// assign and set the zoom increment percentage
+wheelzoom(document.querySelectorAll('img'), {zoom: 0.05});
+
+// set a maximum zoom amount
+wheelzoom(document.querySelectorAll('img'), {maxZoom: 2});
+
+// zooming out can be triggered by calling 'wheelzoom.reset'
+document.querySelector('img').dispatchEvent(new CustomEvent('wheelzoom.reset'));
+
+// wheelzoom can be removed from an element by calling 'wheelzoom.destroy'
+document.querySelector('img').dispatchEvent(new CustomEvent('wheelzoom.destroy'));
