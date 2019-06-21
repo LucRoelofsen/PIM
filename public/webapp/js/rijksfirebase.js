2dbfdd4635fffbf907287c3b5389b01a5daacf3b
AndrewT31 <31801134+AndrewT31@users.noreply.github.com>
2019-06-21 22:31:23 +0200

    Intermediate push for dynamic data
    Co-Authored-By: lucroelofsen <lucroelofsen@users.noreply.github.com>

--- public/webapp/js/rijksfirebase.js
+++ public/webapp/js/rijksfirebase.js
@@ -1,7 +1,9 @@
-// FIREBASE JavaScript
-// ============================
+/*
+RijksCrowd Firebase JavaScript
+Andrew Tan, Luc Roelofsen, Bob Pietersen
+*/
 
-// Initialize FIREBASE
+// Initialize Firebase
 var config = {
   apiKey: "AIzaSyCSjEyjHH51xNwlftWuYKTwzmFyNBLZFX8",
   authDomain: "pim-app-7028c.firebaseapp.com",
@@ -13,6 +15,16 @@ var config = {
 };
 firebase.initializeApp(config);
 var db = firebase.firestore();
+
+
+/*
+ * --------------------------------------------------
+ * Shows real-time data of the transcribing progress
+ * --------------------------------------------------
+*/
+
+// Listen for real-time changes
+db.collection('projects').doc('1').collection('users').onSnapshot(function() { loadProgress(); } );
 
 // Get document name for the preview screen
 window.currentDocPath = [];
@@ -27,24 +39,40 @@ db.collection('projects').doc('1').collection('documents').get().then(snap => {
   window.projectSize = snap.size;
 });
 
+// Get current transcribing progress and show corresponding data
 function loadProgress() {
   db.collection('projects').doc('1').collection('users').get().then((snapshot) => {
     window.userProgress = snapshot.docs[0].data().progress;
   }).then(function() {
     db.collection('projects').doc('1').collection('documents').get().then((snapshot) => {
-      var imageURL = snapshot.docs[userProgress].data().image;
-      var docNumber = userProgress + 1;
-      var showProgress = (userProgress / projectSize) * 100
-      $("#submissionImage").attr("data", imageURL);
-      $("#currentDoc").empty().append("<h3>Document " + docNumber + "/" + projectSize + "</h3>");
-      $("#progressBar").css('width', showProgress + "%");
+
+      // Show transcribing process
+      // Redirect the user to project preview if end of project has been reached
+      if (userProgress < projectSize) {
+        var imageURL = snapshot.docs[userProgress].data().image;
+        var docNumber = userProgress + 1;
+        var showProgress = (userProgress / projectSize) * 100
+        $("#submissionImage").attr("data", imageURL);
+        $("#currentDoc").empty().append("<h3>Document " + docNumber + "/" + projectSize + "</h3>");
+        $("#progressBar").css('width', showProgress + "%");
+      } else {
+        db.collection("projects").doc("1").collection("users").doc("kGlrZAGj4y5INSPFmiqM").update({
+          completed: true
+        });
+        window.location.assign("transcribe_preview.html");
+      };
+
     });
   });
 };
 
-db.collection('projects').doc('1').collection('users').onSnapshot(function() { loadProgress(); } );
 
-loadProgress();
+/*
+------------------------------------------------------------
+Transcribing functionality
+> Form handling on the transcribe page
+------------------------------------------------------------
+*/
 
 // Listen for form submit
 document.getElementById("submissionForm").addEventListener("submit", submitForm);
@@ -74,9 +102,9 @@ function getInputVal(id) {
   return document.getElementById(id).value
 };
 
-// Save message to firebase
+// Save data to Firebase
 function saveSubmission(title, keywordsubject, keywordperson, doctype, date, author) {
-  db.collection("projects").doc("1").collection("documents").doc(currentDocPath[userProgress]).collection("submissions").doc("1").set({
+  db.collection("projects").doc("1").collection("documents").doc(currentDocPath[userProgress]).collection("submissions").doc("1").update({
     title: title,
     keywordsubject: keywordsubject,
     keywordperson: keywordperson,
@@ -84,12 +112,74 @@ function saveSubmission(title, keywordsubject, keywordperson, doctype, date, aut
     date: date,
     author: author
   });
-  loadProgress();
 };
 
-// Update progress in firebase
+// Update progress in Firebase
 function updateProgress() {
-  db.collection("projects").doc("1").collection("users").doc("kGlrZAGj4y5INSPFmiqM").set({
+  db.collection("projects").doc("1").collection("users").doc("kGlrZAGj4y5INSPFmiqM").update({
     progress: userProgress + 1
   });
+
 };
+
+// Handles skipping documents
+document.getElementById("confirmSkip").addEventListener("click", function(){
+  updateProgress();
+})
+
+// Handles reporting illegible documents
+// Future implementation: report to admin
+document.getElementById("confirmIllegible").addEventListener("click", function(){
+  updateProgress();
+})
+
+
+/*
+------------------------------------------------------------
+Transcribe preview
+> Retrieve and show real-time on the preview page
+------------------------------------------------------------
+*/
+
+// // Listen for real-time changes
+// db.collection('projects').doc('1').collection('users').onSnapshot(function() { loadProgress(); } );
+//
+// // Get document name for the preview screen
+// window.currentDocPath = [];
+// db.collection('projects').doc('1').collection('documents').onSnapshot((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     currentDocPath.push(doc.id);
+//   });
+// });
+//
+// // Count the amount of documents in the project
+// db.collection('projects').doc('1').collection('documents').get().then(snap => {
+//   window.projectSize = snap.size;
+// });
+//
+// // Get current transcribing progress and show corresponding data
+// function loadProgress() {
+//   db.collection('projects').doc('1').collection('users').get().then((snapshot) => {
+//     window.userProgress = snapshot.docs[0].data().progress;
+//   }).then(function() {
+//     db.collection('projects').doc('1').collection('documents').get().then((snapshot) => {
+//
+//       // Show transcribing process
+//       // Redirect the user to project preview if end of project has been reached
+//       if (userProgress < projectSize) {
+//         var imageURL = snapshot.docs[userProgress].data().image;
+//         var docNumber = userProgress + 1;
+//         var showProgress = (userProgress / projectSize) * 100
+//         $("#submissionImage").attr("data", imageURL);
+//         $("#currentDoc").empty().append("<h3>Document " + docNumber + "/" + projectSize + "</h3>");
+//         $("#progressBar").css('width', showProgress + "%");
+//       } else {
+//         db.collection("projects").doc("1").collection("users").doc("kGlrZAGj4y5INSPFmiqM").update({
+//           completed: true
+//         });
+//         window.location.replace("transcribe_preview.html");
+//       };
+//
+//     });
+//   });
+// };
